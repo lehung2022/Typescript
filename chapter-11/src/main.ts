480321a3fe27a7dc6988f74fc04227a2e7ec3249
lehung2022 <hung.le.programmer@gmail.com>
2023-10-21 02:28:39 +0700

    fixed something

--- chapter-11/src/main.ts
+++ chapter-11/src/main.ts
@@ -1,48 +1,52 @@
-import './css/style.css'
-import FullList from './model/FullList'
-import ListItem from './model/ListItem'
-import ListTemplate from './templates/ListTemplate'
+import "./css/style.css";
+import FullList from "./model/FullList";
+import ListItem from "./model/ListItem";
+import ListTemplate from "./templates/ListTemplate";
 
 const initApp = (): void => {
-  const fullList = FullList.instance
-  const template = ListTemplate.instance
+  const fullList = FullList.instance;
+  const template = ListTemplate.instance;
 
   // Add listener to new entry form submit
-  const itemEntryForm = document.getElementById("itemEntryForm") as HTMLFormElement
+  const itemEntryForm = document.getElementById(
+    "itemEntryForm"
+  ) as HTMLFormElement;
 
   itemEntryForm.addEventListener("submit", (event: SubmitEvent): void => {
-    event.preventDefault()
+    event.preventDefault();
 
     // Get the new item value
-    const input = document.getElementById("newItem") as HTMLInputElement
-    const newEntryText: string = input.value.trim()
-    if (!newEntryText.length) return
+    const input = document.getElementById("newItem") as HTMLInputElement;
+    const newEntryText: string = input.value.trim();
+    if (!newEntryText.length) return;
 
     // calculate item ID
     const itemId: number = fullList.list.length
       ? parseInt(fullList.list[fullList.list.length - 1].id) + 1
-      : 1
+      : 1;
 
     // create new item
-    const newItem = new ListItem(itemId.toString(), newEntryText)
+    const newItem = new ListItem(itemId.toString(), newEntryText);
     // Add new item to full list
-    fullList.addItem(newItem)
+    fullList.addItem(newItem);
     // Re-render list with new item included
-    template.render(fullList)
-  })
+    template.render(fullList);
+  });
 
   // Add listener to "Clear" button
-  const clearItems = document.getElementById("clearItemsButton") as HTMLButtonElement
+  const clearItems = document.getElementById(
+    "clearItemsButton"
+  ) as HTMLButtonElement;
 
-  clearItems.addEventListener('click', (): void => {
-    fullList.clearList()
-    template.clear()
-  })
+  clearItems.addEventListener("click", (): void => {
+    fullList.clearList();
+    template.clear();
+  });
 
   // load initial data
-  fullList.load()
+  fullList.load();
   // initial render of template
-  template.render(fullList)
-}
+  template.render(fullList);
+};
 
-document.addEventListener("DOMContentLoaded", initApp) 
+document.addEventListener("DOMContentLoaded", initApp);
